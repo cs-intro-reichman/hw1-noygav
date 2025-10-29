9cc9d41f3f40484aad30a785961ee83c3a473446
noygav <noygav@gmail.com>
2025-10-29 18:50:29 +0200

    done print5

--- Print5.java
+++ Print5.java
@@ -1,9 +1,12 @@
 // Prints the numbers 0 to 5.
 // (Same program as shown in lecture 1-1).
-public class PrintTo5 {
+public class Print5{
 	public static void main(String[] args) {
 		// Declares an int variable, and sets it to 1
         int i = 0;
-        // Complete the program's code here:
+        System.out.println("done");
+        System.out.println("main")
+
+;        // Complete the program's code here:
     }
  }
