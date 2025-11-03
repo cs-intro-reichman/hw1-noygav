b95f41e506843d0878387a7f4330b4fa126b7f50
noygav <noygav@gmail.com>
2025-11-03 21:38:09 +0200

    finish numwords

--- NumWords.java
+++ NumWords.java
@@ -2,5 +2,13 @@
 public class NumWords {
 	public static void main(String args[]) {
 	    // Replace this comment with your code
+		int number = Integer.parseInt(args[0]);
+
+		int hundreds = number / 100;
+		int tens = (number % 100) / 10;
+		int units = number % 10;
+
+		System.out.println(hundreds + " hundreds, " + tens + " tens, and " + units + " ones.");
+		// 5 hundreds, 1 tens, and 7 ones.
 	}
 }
